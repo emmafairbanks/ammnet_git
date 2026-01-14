05ce3ccc037adce554e3295c979b13a5bc379fa0
y30282ef <y30282ef@B-LOSXJJ1Q9.local>
2026-01-14 14:09:15 +0000

    Added my script

---
+++ Workshop_live_code.R
@@ -0,0 +1,21 @@
+## Linking Github to Rstudio
+library(usethis)
+
+setwd("~/Desktop/Hackathons/ammnet-hackathon-main/07_github/ammnet_git")
+
+# Connect Git and Github information to R studio
+usethis::git_sitrep()
+
+# Pass Git info to Rstudio
+
+#usethis::gh_token_help()
+
+#usethis::create_github_token()
+
+#gitcreds::gitcreds_set()
+
+usethis::git_sitrep()
+
+#######################
+dir.create('local')
+usethis::use_git_ignore("local")
